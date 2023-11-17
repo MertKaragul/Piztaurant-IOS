3dc38b29039b40d6c05717327f9fff493f112959
Mert Karagul <gmert1702@gmail.com>
2023-11-17 23:20:54 +0300

    Initial Commit

---
+++ PizTaurantIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PizTaurantIOS
+//
+//  Created by Mert Karagül on 17.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PizTaurantIOS/PizTaurantIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizTaurantIOSApp.swift
+//  PizTaurantIOS
+//
+//  Created by Mert Karagül on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct PizTaurantIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
